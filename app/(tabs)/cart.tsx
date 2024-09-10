4557d5578e5b16b7ccecd9b4c225505fb37b0836
eddiemuhoro <eddiemuhoro@gmail.com>
2024-09-10 22:39:41 +0300

    added new screens

---
+++ app/(tabs)/cart.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const cart = () => {
+  return (
+    <View>
+      <Text>cart</Text>
+    </View>
+  );
+};
+
+export default cart;
+
+const styles = StyleSheet.create({});
